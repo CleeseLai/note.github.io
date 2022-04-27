1803c354e019be5484b7c6e93e808088595b202f
CleeseLai <91729077+CleeseLai@users.noreply.github.com>
2022-04-27 16:44:13 +0800

    0427

--- Javap/Javp4_1.java
+++ Javap/Javp4_1.java
@@ -1,1 +1,25 @@
-1000000000000000000000000000000000000000
+public class Javp4_1 
+{ public static void main(String args [ ]) 
+   { 
+      char grade='C';
+ 
+       switch ( grade ) 
+       	{
+       		case'A':
+             System.out.println("優秀"); 
+             break;
+          case'B':
+          case'C':
+             System.out.println("良好");
+            break;
+          case'D':
+             System.out.println("及格");
+             break;
+          case'F':
+             System.out.println("你需要再努力努力");
+             break;
+          default:
+             System.out.println ("未知等級");
+       } System.out.println("你的等級是"+ grade );
+    } 
+}
---
+++ Javap/Javp5_10.java
@@ -0,0 +1,8 @@
+// Javp5_10
+public class Javap5_10
+{
+	public static void main(string args[])
+	{
+		
+	}
+}
